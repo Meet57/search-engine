aa639d1a3958d81469ad9c7b612c0ddd20b3c6cb
meet57 <meet57.mp@gmail.com>
2023-11-21 16:21:27 -0500

    meet init

--- src/components/SearchBoxComponent.js
+++ src/components/SearchBoxComponent.js
@@ -1,77 +1,52 @@
 import React, { useState } from "react";
-import { Select } from "antd";
+import { AutoComplete } from "antd";
+import _debounce from "lodash/debounce";
+import { useEffect } from "react";
 
-const debounce = (func, delay) => {
-  let timeoutId;
-  return function (...args) {
-    if (timeoutId) {
-      clearTimeout(timeoutId);
+const mockVal = (str, repeat = 1) => ({
+  value: str.repeat(repeat),
+});
+
+const SearchBoxComponent = () => {
+  const [value, setValue] = useState("");
+  const [options, setOptions] = useState([]);
+
+  useEffect(() => {
+    debouncedApiCall(value);
+  }, [value])
+
+  const debouncedApiCall = _debounce(async (searchText) => {
+    try {
+      setOptions(!searchText ? [] : [mockVal(searchText), mockVal(searchText, 2), mockVal(searchText, 3)])
+
+      // const response = await fetch(`https://api.example.com/search?q=${searchText}`);
+      // const data = await response.json();
+
+      // setOptions(data.map(item => ({ value: item.name })));
+    } catch (error) {
+      console.error("Error fetching data:", error);
     }
-    timeoutId = setTimeout(() => {
-      func(...args);
-    }, delay);
-  };
-};
-
-const fetch = (value, callback) => {
-  console.log(value);
-
-  const staticData = [
-    { value: "Software Engineer", text: "Software Engineer" },
-    { value: "Data Scientist", text: "Data Scientist" },
-    { value: "UX Designer", text: "UX Designer" },
-    { value: "Product Manager", text: "Product Manager" },
-    { value: "Marketing Specialist", text: "Marketing Specialist" },
-  ];
-
-  setTimeout(() => {
-    callback(staticData);
-  }, 300);
-};
-
-const SearchInput = (props) => {
-  const [data, setData] = useState([]);
-  const [value, setValue] = useState();
-
-  // Debounce the handleSearch function
-  const debouncedHandleSearch = debounce((newValue) => {
-    fetch(newValue, setData);
-    setValue(newValue)
   }, 1000);
 
-  const handleChange = (newValue) => {
-    setValue(newValue);
-    console.log(newValue);
+  const onSelect = (data) => {
+    console.log("onSelect", data);
   };
 
   return (
-    <Select
-      showSearch
-      size="large"
-      value={value}
-      placeholder={props.placeholder}
-      style={props.style}
-      allowClear={true}
-      defaultActiveFirstOption={false}
-      onSearch={debouncedHandleSearch}
-      onChange={handleChange}
-      notFoundContent={null}
-      options={(data || []).map((d) => ({
-        value: d.value,
-        label: d.text,
-      }))}
-    />
+    <>
+      <AutoComplete
+        options={options}
+        value={value}
+        onChange={(text) => setValue(text)}
+        size="large"
+        style={{
+          width: "100%",
+        }}
+        onSelect={onSelect}
+        placeholder="input here"
+      />
+    </>
   );
 };
-
-const SearchBoxComponent = () => (
-  <>
-    <SearchInput
-      placeholder="Enter the Job you are searching for"
-      className="w-100"
-      style={{ width: "100%" }}
-    />
-  </>
-);
 
 export default SearchBoxComponent;
