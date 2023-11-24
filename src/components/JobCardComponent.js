d75284aa3fb318eeab97b40433da9ae291e59b8b
meet57 <meet57.mp@gmail.com>
2023-11-24 18:37:12 -0500

    Location added to card and search css

--- src/components/JobCardComponent.js
+++ src/components/JobCardComponent.js
@@ -1,9 +1,10 @@
 import { Card } from "antd";
 import React from "react";
 import { Button } from "antd";
+import locationIcon from "./../assets/images/location.png";
 
 const JobCardComponent = (props) => {
-  const { id, title, description, link, showLargeDrawer } = props;
+  const { id, title, description, location, showLargeDrawer } = props;
   return (
     <Card
       className="my-3"
@@ -12,6 +13,12 @@ const JobCardComponent = (props) => {
       style={{ width: "100%" }}
       extra={<Button onClick={() => showLargeDrawer(props)}>View More</Button>}
     >
+      <div className="flex">
+        <span>
+          <img src={locationIcon} style={{ width: "20px" }} />
+        </span>
+        <span className="mb-2 ml-1 text-base text-gray-600">{location}</span>
+      </div>
       <p className="description">{description}</p>
     </Card>
   );
